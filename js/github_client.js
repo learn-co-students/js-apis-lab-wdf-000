5d8da7b9c531ee4411961c1652d9176104ac0d27
Wendy Xu <guangw.xu@gmail.com>
2016-11-07 15:03:41 -0500

    Done.

--- js/github_client.js
+++ js/github_client.js
@@ -1,16 +1,61 @@
 //define functions here
+
 var createGist = function(file_name, content, description, token){
+  
+  var gist = {
+    "description": description,
+    "public": true,
+    "files": {
+      [file_name]: {
+        "content": content
+      }
+    }
+  };
+
+  $.ajax({
+    url: 'https://api.github.com/gists',
+    type: 'POST',
+    dataType: 'json',
+    headers: {
+      Authorization: "token " + token
+    },
+    data: JSON.stringify(gist)
+  }).done(function(response) {
+    var username = response.owner.login;
+    myGists(username, token);
+  });
 
 };
 
 var myGists = function (username, token){
-
+  $.ajax({
+    url: 'https://api.github.com/users/' + username + '/gists',
+    type: 'GET',
+    dataType: 'jsonp'
+  }).done(function(gists) {
+    $.each(gists.data, function(index, gist) {
+      var gist_link = gist.html_url
+      var gdescription = gist.description
+      var html = '<a href="' + gist_link + '">' + gdescription + '<\/a>'
+      $("myGists").append(html)
+    });
+  });
 };
 
 var bindCreateButton = function() {
   // call functions here
+  $("form#newGist").submit(function(event) {
+    event.preventDefault();
 
+    var file_name = $("#fileName").val();
+    var content = $("#content").val();
+    var description = $("#description").val();
+    var token = $("#token").val();
+
+    createGist(file_name, content, description, token);
+  });
 };
 
-$(document).ready(function(){
+$(document).ready(function() {
+  bindCreateButton();
 });
